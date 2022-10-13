0ebc3ba615b6ac93949e38652e0b8747e07c5623
wkdyujin <yujin1203713@gmail.com>
2022-10-14 03:09:42 +0900

    bottomnavigation 추가

--- app/src/main/java/org/sopt/sample/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/MainActivity.kt
@@ -2,7 +2,12 @@ package com.sopt.androidpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.sopt.seminar2_test.home.GalleryFragment
+import com.sopt.seminar2_test.home.SearchFragment
+import org.sopt.sample.R
 import org.sopt.sample.databinding.ActivityMainBinding
+import org.sopt.sample.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +18,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        init()
+        // 실습 코드
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.home_container)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.home_container, HomeFragment.newInstance())
+                .commit()
+        }
+
+        onNavSelect()
+//        setProfile()
     }
 
-    private fun init() {
-        binding.profileNameTv.text = "이름: " + intent.getStringExtra("id")
-        binding.profileMbtiTv.text = "MBTI: " + intent.getStringExtra("mbti")
+    private fun onNavSelect() {
+        binding.bnvNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> replaceFragment(HomeFragment())
+                R.id.menu_gallery -> replaceFragment(GalleryFragment())
+                R.id.menu_search -> replaceFragment(SearchFragment())
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.home_container, fragment)
+        transaction.commit()
+    }
+
+    //    private fun setProfile() {
+//        binding.profileNameTv.text = "이름: " + intent.getStringExtra("id")
+//        binding.profileMbtiTv.text = "MBTI: " + intent.getStringExtra("mbti")
+//    }
 }

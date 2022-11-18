28eac083d86417d8ca1c218f2454d005231e7458
wkdyujin <yujin1203713@gmail.com>
2022-11-18 16:04:40 +0900

    [FEAT] SignUp 버튼 활성화 구현

--- app/src/main/java/org/sopt/sample/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/SignUpActivity.kt
@@ -2,12 +2,15 @@ package com.sopt.androidpractice
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.sopt.sample.R
-import org.sopt.sample.data.*
+import org.sopt.sample.data.RequestSignUpDTO
+import org.sopt.sample.data.ResponseSignUpDTO
+import org.sopt.sample.data.ServicePool
 import org.sopt.sample.databinding.ActivitySignUpBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -18,13 +21,13 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private val authService = ServicePool.authService
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         clickSignUp()
+        setClickable()
     }
 
     private fun clickSignUp() {
@@ -44,21 +47,24 @@ class SignUpActivity : AppCompatActivity() {
             )
         ).enqueue(object : Callback<ResponseSignUpDTO> {
             override fun onResponse(
-                call: Call<ResponseSignUpDTO>, response: Response<ResponseSignUpDTO>
+                call: Call<ResponseSignUpDTO>,
+                response: Response<ResponseSignUpDTO>
             ) {
                 if (response.code() == 201) {
                     Log.d("SIGNUP-RESPONSE/SUCCESS", "signup 성공!! , $response")
                     val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
                     intent.putExtra(R.string.id.toString(), binding.etId.text.toString())
                     intent.putExtra(
-                        R.string.password.toString(), binding.etPassword.text.toString()
+                        R.string.password.toString(),
+                        binding.etPassword.text.toString()
                     )
                     intent.putExtra(R.string.name.toString(), binding.etName.text.toString())
                     setResult(RESULT_OK, intent)
                     finish()
                 } else {
                     Log.d(
-                        "SIGNUP-RESPONSE/SUCCESS", "respone: $response"
+                        "SIGNUP-RESPONSE/SUCCESS",
+                        "respone: $response"
                     )
                 }
             }
@@ -66,7 +72,9 @@ class SignUpActivity : AppCompatActivity() {
             override fun onFailure(call: Call<ResponseSignUpDTO>, t: Throwable) {
                 Log.d("SIGNUP-RESPONSE/FAILURE", "signup 실패 ㅠㅠ, ${t.message}")
                 Snackbar.make(
-                    binding.root, R.string.signup_failure, Snackbar.LENGTH_SHORT
+                    binding.root,
+                    R.string.signup_failure,
+                    Snackbar.LENGTH_SHORT
                 ).show()
             }
         })
@@ -85,6 +93,28 @@ class SignUpActivity : AppCompatActivity() {
                     .show()
                 return false
             } else return true
+        }
+    }
+
+    private fun setClickable() {
+        val signUpTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                with(binding) {
+                    btnSignup.isEnabled =
+                        etId.text.isNotEmpty() && etPassword.text.isNotEmpty() && etName.text.isNotEmpty()
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        }
+        with(binding) {
+            etId.addTextChangedListener(signUpTextWatcher)
+            etPassword.addTextChangedListener(signUpTextWatcher)
+            etName.addTextChangedListener(signUpTextWatcher)
         }
     }
 }

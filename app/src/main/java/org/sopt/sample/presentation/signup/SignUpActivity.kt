865b5d69f5d4566c8f66b60dba84e53a2bb55159
wkdyujin <yujin1203713@gmail.com>
2022-11-29 23:40:12 +0900

    [FEAT] signup 조건 설정

--- app/src/main/java/org/sopt/sample/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/signup/SignUpActivity.kt
@@ -2,8 +2,11 @@ package org.sopt.sample.presentation.signup
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import com.google.android.material.snackbar.Snackbar
 import org.sopt.sample.R
 import org.sopt.sample.databinding.ActivitySignUpBinding
@@ -12,14 +15,55 @@ import org.sopt.sample.presentation.login.LoginActivity
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
-    private val viewmodel by viewModels<SignUpViewModel>()
+    private val viewModel by viewModels<SignUpViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnSignup.isEnabled = true
         clickSignUp()
+        setCheckId()
+        setCheckPassword()
+    }
+
+    private fun setCheckId() {
+        binding.etId.doAfterTextChanged { text ->
+            viewModel.checkPattern(text.toString(), 0)
+        }
+        viewModel.checkId.observe(this) { check ->
+            with(binding) {
+                if (check) {
+                    tvIdErrorMsg.visibility = View.GONE
+                    etId.backgroundTintList =
+                        ContextCompat.getColorStateList(applicationContext, R.color.teal_200)
+                } else {
+                    tvIdErrorMsg.visibility = View.VISIBLE
+                    etId.backgroundTintList =
+                        ContextCompat.getColorStateList(applicationContext, R.color.red)
+                }
+            }
+            setLoginBtn()
+        }
+    }
+
+    private fun setCheckPassword() {
+        binding.etPassword.doAfterTextChanged { text ->
+            viewModel.checkPattern(text.toString(), 1)
+        }
+        viewModel.checkPassword.observe(this) { check ->
+            with(binding) {
+                if (check) {
+                    tvPasswordErrorMsg.visibility = View.GONE
+                    etPassword.backgroundTintList =
+                        ContextCompat.getColorStateList(applicationContext, R.color.teal_200)
+                } else {
+                    tvPasswordErrorMsg.visibility = View.VISIBLE
+                    etPassword.backgroundTintList =
+                        ContextCompat.getColorStateList(applicationContext, R.color.red)
+                }
+            }
+            setLoginBtn()
+        }
     }
 
     private fun clickSignUp() {
@@ -29,12 +73,12 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun signUp() {
-        viewmodel.signUp(
+        viewModel.signUp(
             binding.etId.text.toString(),
             binding.etPassword.text.toString(),
             binding.etName.text.toString()
         )
-        viewmodel.successSignUp.observe(this) { success ->
+        viewModel.successSignUp.observe(this) { success ->
             if (success) {
                 val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
                 intent.putExtra(R.string.id.toString(), binding.etId.text.toString())
@@ -45,5 +89,10 @@ class SignUpActivity : AppCompatActivity() {
                 Snackbar.make(binding.root, R.string.signup_failure, Snackbar.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun setLoginBtn() {
+        binding.btnSignup.isEnabled =
+            viewModel.checkId.value == true && viewModel.checkPassword.value == true
     }
 }
--- app/src/main/java/org/sopt/sample/presentation/signup/SignUpViewModel.kt
+++ app/src/main/java/org/sopt/sample/presentation/signup/SignUpViewModel.kt
@@ -15,6 +15,16 @@ class SignUpViewModel : ViewModel() {
     private val _successSignUp = MutableLiveData<Boolean>()
     val successSignUp: LiveData<Boolean> = _successSignUp
 
+    private val _checkId = MutableLiveData<Boolean>()
+    val checkId: LiveData<Boolean> = _checkId
+
+    private val _checkPassword = MutableLiveData<Boolean>()
+    val checkPassword: LiveData<Boolean> = _checkPassword
+
+    private val idPattern = Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,10}\$")
+    private val passwordPattern =
+        Regex("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#\$%^&*()+|=])[A-Za-z\\d~!@#\$%^&*()+|=]{6,12}\$")
+
     fun signUp(id: String, password: String, name: String) {
         authService.signup(
             RequestSignUpDTO(id, password, name)
@@ -36,5 +46,17 @@ class SignUpViewModel : ViewModel() {
                 Log.d("SignUP 서버 통신 실패", t.message.toString())
             }
         })
+    }
+
+    fun checkPattern(text: String, type: Int) {
+        when (type) {
+            ID -> _checkId.value = (text.length in 6..10 && idPattern.matches(text)) || text.isEmpty()
+            PASSWORD -> _checkPassword.value = (text.length in 6..12 && passwordPattern.matches(text)) || text.isEmpty()
+        }
+    }
+
+    companion object {
+        private val ID = 0
+        private val PASSWORD = 1
     }
 }

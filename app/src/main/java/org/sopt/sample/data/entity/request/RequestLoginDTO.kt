07d3850f1ed7ce7e5568eb67621284a9c9c6b150
wkdyujin <yujin1203713@gmail.com>
2022-11-30 10:45:30 +0900

    [DEL] SerialName 어노테이션 삭제

--- app/src/main/java/org/sopt/sample/data/entity/request/RequestLoginDTO.kt
+++ app/src/main/java/org/sopt/sample/data/entity/request/RequestLoginDTO.kt
@@ -1,13 +1,9 @@
 package org.sopt.sample.data.entity.request
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RequestLoginDTO(
-    // JSON 객체로 변환될 때 'email': 'nunu.lee@gmail.com' 과 같은 식으로 들어갈 수 있다는 표기
-    @SerialName("email")
     val email: String,
-    @SerialName("password")
     val password: String
 )
--- app/src/main/java/org/sopt/sample/data/entity/response/ResponseUserInfoDTO.kt
+++ app/src/main/java/org/sopt/sample/data/entity/response/ResponseUserInfoDTO.kt
@@ -1,42 +1,28 @@
 package org.sopt.sample.data.entity.response
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseUserInfoDTO(
-    @SerialName("page")
     val page: Int,
-    @SerialName("per_page")
     val per_page: Int,
-    @SerialName("total")
     val total: Int,
-    @SerialName("total_pages")
     val total_pages: Int,
-    @SerialName("data")
     val data: List<UserInfo>,
-    @SerialName("support")
     val support: Support
 ) {
     @Serializable
     data class UserInfo(
-        @SerialName("id")
         val id: Int,
-        @SerialName("email")
         val email: String,
-        @SerialName("first_name")
         val first_name: String,
-        @SerialName("last_name")
         val last_name: String,
-        @SerialName("avatar")
         val avatar: String
     )
 
     @Serializable
     data class Support(
-        @SerialName("url")
         val url: String,
-        @SerialName("text")
         val text: String
     )
 }
